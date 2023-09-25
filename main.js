d2befb2f8eeee4e107bdb0e28a88b253c670fede
Pirisinu <palazzialessandro2@gmail.com>
2023-09-25 14:31:41 +0200

    [ADD]POST IN HTML

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+let container = document.getElementById('container');
 const posts = [
     {
         "id": 1,
@@ -58,6 +58,40 @@ const posts = [
 ];
 
 for (post of posts) {
-
+    post = `<div class="post">
+    <div class="post__header">
+      <div class="post-meta">
+        <div class="post-meta__icon">
+          <img class="profile-pic" src="${post.author[1]}" alt="${post.name}">
+        </div>
+        <div class="post-meta__data">
+          <div class="post-meta__author">${post.name}</div>
+          <div class="post-meta__time">${post.created}</div>
+        </div>
+      </div>
+    </div>
+    <div class="post__text">
+      ${post.content}
+    </div>
+    <div class="post__image">
+      <img src="${post.media}" alt>
+    </div>
+    <div class="post__footer">
+      <div class="likes js-likes">
+        <div class="likes__cta">
+          <a class="like-button js-like-button" data-postid="1" href="#">
+            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+            <span class="like-button__label">Mi Piace</span>
+          </a>
+        </div>
+        <div class="likes__counter">
+          Piace a
+          <b class="js-likes-counter" id="like-counter-${post.id}">${post.likes}</b> persone
+        </div>
+      </div>
+    </div>
+  </div>`
+  container.innerHTML += post;
     console.log(post);
+
 }
